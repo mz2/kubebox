1b3485f4d245bb09d5738174fccd2e61d84ceb88
Matias Piipari <matias.piipari@gmail.com>
2022-08-20 15:11:45 +0300

    Adding a VirtualMachine declaration

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+On host system:
+
 ```bash
-./cluster-cloudconfig.py | multipass launch 22.04 -c 4 -d 10G -m 32G -n kubebox -vvvv --cloud-init -
+./cluster-cloudconfig.py | multipass launch 22.04 -c 4 -d 50G -m 32G -n kubebox -vvvv --cloud-init -
+multipass mount . kubebox:/kubebox
+```
+
+```bash
+multipass shell kubebox
+cd /kubebox
+./kubevirt.sh
 ```
--- kubevirt.sh
+++ kubevirt.sh
@@ -10,9 +10,10 @@ set -e pipefail
   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
   tar zxvf "${KREW}.tar.gz" &&
   ./"${KREW}" install krew
-  echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> .bash_profile
-  source ~/.bash_profile
 )
+
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' > ~/.bash_profile
+source ~/.bash_profile
 
 kubectl krew install virt
 
